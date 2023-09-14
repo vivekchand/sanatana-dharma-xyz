01fd04efb61deb694a56b2b7f8e1998d0c66fcbe
Vivek Chand <vivekchand19@gmail.com>
2023-09-14 22:04:50 +0000

    remove color

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
-      <div className="font-bold text-3xl flex items-center" style={{color: "#F47216"}}>
+      <div className="font-bold text-3xl flex items-center">
         <a
           className="ml-2 hover:opacity-50"
           href="https://sanatanadharma.xyz"
