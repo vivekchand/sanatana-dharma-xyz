e7dd437de2be9db1dca68cd32824fef25856513c
Vivek Chand <vivekchand19@gmail.com>
2023-09-20 18:29:52 +0000

    update

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -8,67 +8,55 @@ const PrivacyPolicy = () => {
         <p>Last Updated: 20 Sep 2023</p>
 
         <p>
-            This Privacy Policy describes how SanatanaDharma.xyz (&quot;we,&quot; &quot;our,&quot; or &quot;us&quot;) collects, uses, and shares information when you use our website. By using the website, you consent to the practices described in this Privacy Policy.
+          This Privacy Policy describes how SanatanaDharma.xyz (&quot;we,&quot; &quot;our,&quot; or &quot;us&quot;) does not collect, store, or log any information when you use our website. By using the website, you consent to the practices described in this Privacy Policy.
         </p>
+        <br/>
+        <h2 className="text-xl font-semibold mb-2">1. Information We Do Not Collect</h2>
 
-        <h2 className="text-xl font-semibold mb-2">1. Information We Collect</h2>
-
-        <h3>1.1 Chat Interactions</h3>
         <p>
-          When you interact with SanatanaDharma.xyz, we collect the messages exchanged during the chat session. This may include any personal information or inquiries you provide voluntarily.
+          We do not collect any information when you interact with SanatanaDharma.xyz. We do not store or log messages exchanged during the chat session, personal information, or any other data.
         </p>
 
-        <h3>1.2 Usage Information</h3>
+        <br/>
+        <h2 className="text-xl font-semibold mb-2">2. How We Do Not Use Your Information</h2>
         <p>
-          We may collect usage information, such as your IP address, browser type, and device information, to improve the functionality and performance of the website.
+          Since we do not collect any information, we do not use it for any purposes.
         </p>
 
-        <h2 className="text-xl font-semibold mb-2">2. How We Use Your Information</h2>
+        <br/>
+        <h2 className="text-xl font-semibold mb-2">3. How We Do Not Share Your Information</h2>
         <p>
-          We may use the information we collect for the following purposes:
+          Since we do not collect any information, we do not share it with anyone.
         </p>
-        <ul>
-          <li>To provide and maintain the website service.</li>
-          <li>To improve the website functionality and user experience.</li>
-          <li>To respond to your inquiries and requests.</li>
-          <li>To send you updates and notifications related to SanatanaDharma.xyz.</li>
-          <li>To comply with legal obligations.</li>
-        </ul>
 
-        <h2 className="text-xl font-semibold mb-2">3. How We Share Your Information</h2>
-        <p>
-          We do not sell or rent your personal information to third parties. However, we may share your information in the following circumstances:
-        </p>
-        <ul>
-          <li>With service providers who assist us in providing the website service.</li>
-          <li>To comply with legal obligations, such as responding to court orders or other legal processes.</li>
-          <li>To protect our rights, privacy, safety, or property, as well as that of our users and the public.</li>
-          <li>In connection with a merger, acquisition, or sale of all or a portion of our assets, if applicable.</li>
-        </ul>
-
+        <br/>
         <h2 className="text-xl font-semibold mb-2">4. Cookies and Similar Technologies</h2>
         <p>
-          We may use cookies and similar technologies to collect information about your interactions with the website. You can manage your cookie preferences through your browser settings.
+          We do not use cookies or similar technologies to collect information about your interactions with the website. We respect your privacy and do not track your online activities.
         </p>
 
+        <br/>
         <h2 className="text-xl font-semibold mb-2">5. Your Choices</h2>
         <p>
-          You can choose not to provide us with certain information, but it may limit your ability to use certain features of the website.
+          You can use our website without any concerns about providing personal information since we do not collect any data.
         </p>
 
+        <br/>
         <h2 className="text-xl font-semibold mb-2">6. Data Security</h2>
         <p>
-          We take reasonable measures to protect your personal information, but please be aware that no security measures are completely foolproof.
+          We take reasonable measures to protect your privacy, even though we do not collect or store any information. Please be aware that no security measures are completely foolproof, but we strive to ensure your online safety.
         </p>
 
+        <br/>
         <h2 className="text-xl font-semibold mb-2">7. Changes to this Privacy Policy</h2>
         <p>
-          We may update this Privacy Policy from time to time. We will notify you of any significant changes by posting the new Privacy Policy on this page.
+          Since our privacy policy states that we do not collect or store any data, there will be no changes to this aspect of our policy.
         </p>
 
+        <br/>
         <h2 className="text-xl font-semibold mb-2">8. Contact Us</h2>
         <p>
-          If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:info@sanatanadharma.xyz">info@sanatanadharma.xyz</a>.
+          If you have any questions or concerns about our privacy policy, please contact us at <a href="mailto:info@sanatanadharma.xyz">info@sanatanadharma.xyz</a>.
         </p>
       </div>
     </>
