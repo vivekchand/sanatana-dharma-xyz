f2a7f630bc8d244b9a75bf6f00437e8c77eda068
Vivek Chand <vivekchand19@gmail.com>
2023-09-20 18:01:13 +0000

    add privacy policy

---
+++ pages/privacy.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <div className="container mx-auto py-8">
+        <h1 className="text-2xl font-semibold mb-4">Privacy Policy</h1>
+        <p className="mb-4">
+          This Privacy Policy describes how we collect, use, and share your
+          personal information when you use our services.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Information We Collect</h2>
+        <p className="mb-4">
+          We collect information that you provide to us voluntarily, such as
+          your name and email address when you contact us or use our services.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">How We Use Your Information</h2>
+        <p className="mb-4">
+          We use the information we collect to provide and improve our services,
+          respond to your inquiries, and send you updates and notifications.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">How We Share Your Information</h2>
+        <p className="mb-4">
+          We do not sell or rent your personal information to third parties. We
+          may share your information with service providers who assist us in
+          providing our services or to comply with legal obligations.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Cookies and Similar Technologies</h2>
+        <p className="mb-4">
+          We may use cookies and similar technologies to collect information
+          about your interactions with our services. You can manage your cookie
+          preferences through your browser settings.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Your Choices</h2>
+        <p className="mb-4">
+          You can choose not to provide certain information, but it may limit
+          your ability to use certain features of our services.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Data Security</h2>
+        <p className="mb-4">
+          We take reasonable measures to protect your personal information, but
+          no security measures are completely foolproof.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Changes to this Privacy Policy</h2>
+        <p className="mb-4">
+          We may update this Privacy Policy from time to time. We will notify
+          you of any significant changes by posting the new Privacy Policy on
+          this page.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">Contact Us</h2>
+        <p>
+          If you have any questions or concerns about this Privacy Policy,
+          please contact us at{" "}
+          <a href="mailto:contact@email.com">contact@email.com</a>.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
