ab1cca9fda8b51b34e7c660c582489f16e196557
Vivek Chand <vivekchand19@gmail.com>
2023-09-20 18:06:34 +0000

    update privacy

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -4,62 +4,71 @@ const PrivacyPolicy = () => {
   return (
     <>
       <div className="container mx-auto py-8">
-        <h1 className="text-2xl font-semibold mb-4">Privacy Policy</h1>
-        <p className="mb-4">
-          This Privacy Policy describes how we collect, use, and share your
-          personal information when you use our services.
-        </p>
+        <h1 className="text-2xl font-semibold mb-4">Privacy Policy for SanatanaDharma.xyz</h1>
+        <p>Last Updated: [Date]</p>
 
-        <h2 className="text-xl font-semibold mb-2">Information We Collect</h2>
-        <p className="mb-4">
-          We collect information that you provide to us voluntarily, such as
-          your name and email address when you contact us or use our services.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">How We Use Your Information</h2>
-        <p className="mb-4">
-          We use the information we collect to provide and improve our services,
-          respond to your inquiries, and send you updates and notifications.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">How We Share Your Information</h2>
-        <p className="mb-4">
-          We do not sell or rent your personal information to third parties. We
-          may share your information with service providers who assist us in
-          providing our services or to comply with legal obligations.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Cookies and Similar Technologies</h2>
-        <p className="mb-4">
-          We may use cookies and similar technologies to collect information
-          about your interactions with our services. You can manage your cookie
-          preferences through your browser settings.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Your Choices</h2>
-        <p className="mb-4">
-          You can choose not to provide certain information, but it may limit
-          your ability to use certain features of our services.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Data Security</h2>
-        <p className="mb-4">
-          We take reasonable measures to protect your personal information, but
-          no security measures are completely foolproof.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Changes to this Privacy Policy</h2>
-        <p className="mb-4">
-          We may update this Privacy Policy from time to time. We will notify
-          you of any significant changes by posting the new Privacy Policy on
-          this page.
-        </p>
-
-        <h2 className="text-xl font-semibold mb-2">Contact Us</h2>
         <p>
-          If you have any questions or concerns about this Privacy Policy,
-          please contact us at{" "}
-          <a href="mailto:contact@email.com">contact@email.com</a>.
+          This Privacy Policy describes how SanatanaDharma.xyz ("we," "our," or "us") collects, uses, and shares information when you use our website. By using the website, you consent to the practices described in this Privacy Policy.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">1. Information We Collect</h2>
+
+        <h3>1.1 Chat Interactions</h3>
+        <p>
+          When you interact with SanatanaDharma.xyz, we collect the messages exchanged during the chat session. This may include any personal information or inquiries you provide voluntarily.
+        </p>
+
+        <h3>1.2 Usage Information</h3>
+        <p>
+          We may collect usage information, such as your IP address, browser type, and device information, to improve the functionality and performance of the website.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">2. How We Use Your Information</h2>
+        <p>
+          We may use the information we collect for the following purposes:
+        </p>
+        <ul>
+          <li>To provide and maintain the website service.</li>
+          <li>To improve the website's functionality and user experience.</li>
+          <li>To respond to your inquiries and requests.</li>
+          <li>To send you updates and notifications related to SanatanaDharma.xyz.</li>
+          <li>To comply with legal obligations.</li>
+        </ul>
+
+        <h2 className="text-xl font-semibold mb-2">3. How We Share Your Information</h2>
+        <p>
+          We do not sell or rent your personal information to third parties. However, we may share your information in the following circumstances:
+        </p>
+        <ul>
+          <li>With service providers who assist us in providing the website service.</li>
+          <li>To comply with legal obligations, such as responding to court orders or other legal processes.</li>
+          <li>To protect our rights, privacy, safety, or property, as well as that of our users and the public.</li>
+          <li>In connection with a merger, acquisition, or sale of all or a portion of our assets, if applicable.</li>
+        </ul>
+
+        <h2 className="text-xl font-semibold mb-2">4. Cookies and Similar Technologies</h2>
+        <p>
+          We may use cookies and similar technologies to collect information about your interactions with the website. You can manage your cookie preferences through your browser settings.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">5. Your Choices</h2>
+        <p>
+          You can choose not to provide us with certain information, but it may limit your ability to use certain features of the website.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">6. Data Security</h2>
+        <p>
+          We take reasonable measures to protect your personal information, but please be aware that no security measures are completely foolproof.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">7. Changes to this Privacy Policy</h2>
+        <p>
+          We may update this Privacy Policy from time to time. We will notify you of any significant changes by posting the new Privacy Policy on this page.
+        </p>
+
+        <h2 className="text-xl font-semibold mb-2">8. Contact Us</h2>
+        <p>
+          If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:contact@email.com">contact@email.com</a>.
         </p>
       </div>
     </>
