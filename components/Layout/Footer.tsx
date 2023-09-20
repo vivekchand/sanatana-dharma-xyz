739f8ad3e7f7a6c6281a2581d5602c37d9463d5a
Vivek Chand <vivekchand19@gmail.com>
2023-09-20 18:50:35 +0000

    add google analytics

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,12 +1,10 @@
-import { FC } from "react";
-import Link from "next/link";
+import ReactGA from "react-ga";
 
-export const Footer: FC = () => {
-  return (
-    <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center">
-      <Link href="/privacy">
-        <a className="text-neutral-500 hover:text-primary-500">Privacy Policy</a>
-      </Link>
-    </div>
-  );
+export const initGA = () => {
+  ReactGA.initialize("G-916S3MSTVF");
+};
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
 };
