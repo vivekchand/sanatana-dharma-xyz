042fe1fee8d7bb9f8a787c6a0813bb50455846d1
Vivek Chand <vivekchand19@gmail.com>
2023-09-20 18:39:07 +0000

    update

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react";
+import Link from "next/link";
 
 export const Footer: FC = () => {
-  return <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center"></div>;
+  return (
+    <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center">
+      <Link href="/privacy">
+        <a className="text-neutral-500 hover:text-primary-500">Privacy Policy</a>
+      </Link>
+    </div>
+  );
 };
