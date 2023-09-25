493d5279c769044cada641b21fcfba6fc8920fc4
Vivek Chand <vivekchand19@gmail.com>
2023-09-25 21:35:48 +0000

    fix height

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer: FC = () => {
   return (
-    <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center">
+    <div className="flex h-[50px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center">
       <Link href="/privacy">
         <a className="text-neutral-500 hover:text-primary-500">PrivacyPolicy</a>
       </Link>
