201869717755b3fbee865ab65dd8a6301fccc981
Vivek Chand <vivekchand19@gmail.com>
2023-09-29 20:30:42 +0000

    update ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -212,15 +212,15 @@ export default function Home() {
               </p>
               <div className="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
-                <p className="text-black-500">Daily Inspiration</p>
+                <p className="text-black-500">&nbsp;Daily Inspiration</p>
               </div>
               <div className="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
-                <p className="text-black-500">Deeper Cultural Connection</p>
+                <p className="text-black-500">&nbsp;Deeper Cultural Connection</p>
               </div>
               <div className="flex items-center mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
-                <p className="text-black-500">Path to Spiritual Enlightenment</p>
+                <p className="text-black-500">&nbsp;Path to Spiritual Enlightenment</p>
               </div>
               <input
                 type="email"
