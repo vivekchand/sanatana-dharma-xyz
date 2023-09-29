290ce8069e5c7a3d34e9cca99d0006844d70c903
Vivek Chand <vivekchand19@gmail.com>
2023-09-29 20:29:56 +0000

    update ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -211,15 +211,16 @@ export default function Home() {
                 Welcome to a journey of spiritual growth! Subscribe to our newsletter to receive daily Bhagavad Gita verses, ancient wisdom from Vedas, Upanishads, and more, directly in your inbox.
               </p>
               <div className="flex items-center mb-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                <p className="text-green-500">Daily Inspiration</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
+                <p className="text-black-500">Daily Inspiration</p>
               </div>
               <div className="flex items-center mb-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                <p className="text-green-500">Daily Inspiration</p>
-                <p className="text-green-500">Deeper Cultural Connection</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
+                <p className="text-black-500">Deeper Cultural Connection</p>
               </div>
               <div className="flex items-center mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                <p className="text-green-500">Daily Inspiration</p>
-                <p className="text-green-500">Path to Spiritual Enlightenment</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
+                <p className="text-black-500">Path to Spiritual Enlightenment</p>
               </div>
               <input
                 type="email"
