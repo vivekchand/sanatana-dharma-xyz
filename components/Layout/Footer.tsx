9fb56ea21c31d0c2f60dac3b5d47fd8341cf0146
Vivek Chand <vivekchand19@gmail.com>
2023-09-29 21:02:26 +0000

    update ui

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -16,9 +16,9 @@ export const Footer: FC = () => {
 
   return (
     <div className="flex h-[50px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center">
-      <Link href="/privacy">
+      {/* <Link href="/privacy">
         <a className="text-neutral-500 hover:text-primary-500">Privacy Policy</a>
-      </Link>
+      </Link> */}
       <a
         onClick={handleOpenSubscriptionPopup}
         className="text-neutral-500 hover:text-primary-500 cursor-pointer"
