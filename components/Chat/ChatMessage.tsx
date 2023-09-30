59632632d5e41ffcf401018d2c40d277afff048c
Vivek Chand <vivekchand19@gmail.com>
2023-09-30 22:26:26 +0000

    fix css

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -10,7 +10,7 @@ export const ChatMessage: FC<Props> = ({ message }) => {
     <div className={`flex flex-col ${message.role === "assistant" ? "items-start" : "items-end"}`}>
       <div
         className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-white" : "bg-blue-500 text-neutral-900"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
-        style={{ overflowWrap: "anywhere", backgroundColor: message.role === "assistant" ? "#624f96" : "#F4C430", maxWidth: '77%', fontSize: 'smaller' }}
+        style={{ overflowWrap: "anywhere", backgroundColor: message.role === "assistant" ? "#624f96" : "#F4C430", maxWidth: '77%' }}
       >
         {message.content}
       </div>
