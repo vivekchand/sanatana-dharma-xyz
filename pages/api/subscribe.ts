a9c7b45c4b9ae7abaf83269b4147406cd1f4c9c7
Vivek Chand <vivekchand19@gmail.com>
2023-09-30 22:45:24 +0000

    update

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,17 +1,44 @@
 import { OpenAIStream } from "@/utils";
 import { kv } from "@vercel/kv";
+import { Mutex } from "async-mutex";
 
 export const config = {
   runtime: "edge"
 };
+const emailMutex = new Mutex();
 
 const handler = async (req: Request): Promise<Response> => {
   try {
-    // Parse the request body to get the messages and requestId
+    // Parse the request body to get the email
     const requestBody = await req.json();
     const { email } = requestBody;
 
-    return new Response("subscribed!");
+    try {
+      // Acquire a lock to ensure exclusive access to the emails list
+      const release = await emailMutex.acquire();
+
+      try {
+        // Get the current list of emails or initialize an empty array
+        const emails = (await kv.get("emails")) || [];
+
+        // Append the new email to the list
+        emails.push(email);
+
+        // Store the updated list of emails back in the key-value store
+        await kv.put("emails", JSON.stringify(emails));
+
+        console.log(email);
+        // Handle the successful response here
+      } finally {
+        // Release the lock
+        release();
+      }
+    } catch (error) {
+      console.error("Error updating emails list:", error);
+      // Handle the error as needed
+    }
+
+    return new Response("Subscribed!");
   } catch (error) {
     console.error(error);
     return new Response("Error", { status: 500 });
