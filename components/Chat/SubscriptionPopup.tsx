3f30e011cecdffa55b7c498798a6a39021d72f54
Vivek Chand <vivekchand19@gmail.com>
2023-10-01 06:37:41 +0000

    update image

--- components/Chat/SubscriptionPopup.tsx
+++ components/Chat/SubscriptionPopup.tsx
@@ -24,15 +24,15 @@ export const SubscriptionPopup: FC<SubscriptionPopupProps> = ({ onClose, onSubsc
         onSubscribed();
     };
     
-   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
             <div className="max-w-[400px] text-center"> {/* Adjust the max-width as needed */}
               <img src="https://krishnastore.com/images/cache/1614.jpg" alt="Krishna" className="mb-4 max-h-40 w-auto" />
               <h2 className="text-xl font-semibold mb-2">🌞 Journey to Wisdom 📖</h2>
               <p className="text-sm text-gray-600 mb-4">
-              Begin an enlightening journey! Subscribe to our newsletter for daily Bhagavad Gita verses and profound wisdom in your inbox.
+                Begin an enlightening journey! Subscribe to our newsletter for daily Bhagavad Gita verses and profound wisdom in your inbox.
               </p>
               <div className="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>                
@@ -52,7 +52,7 @@ export const SubscriptionPopup: FC<SubscriptionPopupProps> = ({ onClose, onSubsc
                   placeholder="Email address"
                   className="border p-2 rounded-md w-full"
                   onChange={(e) => setEmail(e.target.value)} // Capture email input value
-                  />
+                />
               </div>
               <div className="flex flex-col sm:flex-row"> {/* Buttons in separate rows */}
                 <button
@@ -71,6 +71,6 @@ export const SubscriptionPopup: FC<SubscriptionPopupProps> = ({ onClose, onSubsc
             </div>
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
 };
