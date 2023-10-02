d361423ddf0182cb24e34174db0a2eb1fff0e125
Vivek Chand <vivekchand19@gmail.com>
2023-10-02 07:07:38 +0000

    remove bounce

--- components/Chat/SubscribedPopup.tsx
+++ components/Chat/SubscribedPopup.tsx
@@ -8,7 +8,7 @@ export const SubscribedPopup: FC<SubscribedPopupProps> = ({ onClose }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-4 rounded-lg shadow-md text-center">
-        <div className="animate-bounce text-green-500 text-4xl mb-2">
+        <div className="text-green-500 text-4xl mb-2">
           {/* Big checkmark (tick) */}
           <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="71" height="71" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="green"></circle><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"></path></g></svg>
