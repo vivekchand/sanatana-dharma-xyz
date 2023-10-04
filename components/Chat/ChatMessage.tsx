5dc958abd4bc22172a57b326287e12b1cc216059
Vivek Chand <vivekchand19@gmail.com>
2023-10-04 15:24:50 +0000

    swap colors

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -9,7 +9,7 @@ export const ChatMessage: FC<Props> = ({ message }) => {
   return (
     <div className={`flex flex-col ${message.role === "assistant" ? "items-start" : "items-end"}`}>
       <div
-        className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-white" : "bg-blue-500 text-neutral-900"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
+        className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-neutral-900" : "bg-blue-500 text-black"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
         style={{ overflowWrap: "anywhere", maxWidth: '77%' }}
       >
         {message.content}
