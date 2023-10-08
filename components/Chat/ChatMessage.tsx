adf6b82f9e28c6a4def8ae99aecfd27928745863
Vivek Chand <vivekchand19@gmail.com>
2023-10-08 11:48:45 +0000

    change colors

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -9,8 +9,8 @@ export const ChatMessage: FC<Props> = ({ message }) => {
   return (
     <div className={`flex flex-col ${message.role === "assistant" ? "items-start" : "items-end"}`}>
       <div
-        className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-neutral-900" : "bg-blue-500 text-white"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
-        style={{ overflowWrap: "anywhere", maxWidth: '77%' }}
+        className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-white" : "bg-blue-500 text-white"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
+        style={{ overflowWrap: "anywhere", maxWidth: '77%', background: message.role === "assistant"? "purple": "orange", color: message.role === "assistant"? "wheat": "black" }}
       >
         {message.content}
       </div>
