09e17126a184a47503c62132119dc197e81f166e
Vivek Chand <vivekchand19@gmail.com>
2023-10-08 11:09:03 +0000

    show text ad

--- pages/index.tsx
+++ pages/index.tsx
@@ -192,6 +192,13 @@ export default function Home() {
         <Navbar />
         <div className="flex-1 overflow-auto sm:px-10 pb-4 sm:pb-10" style={{backgroundColor: "#f9f4f0"}}>
           <div className="mx-auto mt-4 sm:mt-12">
+          <Chat
+              messages={messages}
+              loading={loading}
+              onSend={handleSend}
+              onReset={handleReset}
+            />
+            <div ref={messagesEndRef} />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6557627020167681"
             crossOrigin="anonymous"></script>
           <ins className="adsbygoogle"
@@ -205,14 +212,6 @@ export default function Home() {
               (adsbygoogle = window.adsbygoogle || []).push({});
             `}
           </script>
-          <Chat
-              messages={messages}
-              loading={loading}
-              onSend={handleSend}
-              onReset={handleReset}
-            />
-
-            <div ref={messagesEndRef} />
           </div>
         </div>
         <Footer />
