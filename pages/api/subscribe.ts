14f715b399b382d11c501fb13357c99fe17e38a7
Vivek Chand <vivekchand19@gmail.com>
2023-10-09 23:23:04 +0000

    add more exclamation

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -66,7 +66,7 @@ const handler = async (req: Request): Promise<Response> => {
       .then(data => console.log(data))
       .catch(error => console.error('Error:', error));
     
-    return new Response("Subscribed!");
+    return new Response("Subscribed!!!");
   } catch (error) {
     console.error(error);
     return new Response("Error", { status: 500 });
