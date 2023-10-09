743acfa11badfb3d1bbfbda527d0116893bd8d5a
Vivek Chand <vivekchand19@gmail.com>
2023-10-09 23:10:51 +0000

    update call

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,6 +1,10 @@
 import { OpenAIStream } from "@/utils";
 import { kv } from "@vercel/kv";
 import { Mutex } from "async-mutex";
+import twilio from "twilio";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 interface TwilioMessage {
   sid: string;
@@ -44,6 +48,24 @@ const handler = async (req: Request): Promise<Response> => {
       // Handle the error as needed
     }
 
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${Buffer.from(`${accountSid}:${authToken}`).toString('base64')}`,
+      },
+      body: new URLSearchParams({
+        'From': "whatsapp:+14155238886",
+        'To': "whatsapp:+15005550006",
+        'Body': "Hello there!",
+      }),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+    
     return new Response("Subscribed!");
   } catch (error) {
     console.error(error);
