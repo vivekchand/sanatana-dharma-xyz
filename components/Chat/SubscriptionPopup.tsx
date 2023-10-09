48023eff801de240f0bbf108fc99e22da93d2477
Vivek Chand <vivekchand19@gmail.com>
2023-10-09 16:54:46 +0000

    subscribe now

--- components/Chat/SubscriptionPopup.tsx
+++ components/Chat/SubscriptionPopup.tsx
@@ -83,7 +83,7 @@ export const SubscriptionPopup: FC<SubscriptionPopupProps> = ({ onClose, onSubsc
                 className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 mb-2 sm:mb-0 sm:mr-2"
                 onClick={handleSubscribe}
               >
-                Subscribe Now for ₹49/month
+                Subscribe Now
               </button>
               <button
                 className="text-gray-500 text-sm hover:text-gray-700"
