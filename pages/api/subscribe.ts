b3c7c3e1e4bbf1aaeae40e0d2cbc1ecb62701efe
Vivek Chand <vivekchand19@gmail.com>
2023-10-12 19:21:58 +0000

    update message

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -21,15 +21,15 @@ async function sendWhatsappMessage(url:string, email:string) {
   try {
     const message = `🙏 Om Gam Ganapataye Namaha 🐘
 
-    On this auspicious day, let us begin our journey by offering our heartfelt prayers to Lord Ganapati, the remover of obstacles and the embodiment of wisdom. May his divine presence guide us on this path of exploration and discovery. 🙏
+On this auspicious day, let us begin our journey by offering our heartfelt prayers to Lord Ganapati, the remover of obstacles and the embodiment of wisdom. May his divine presence guide us on this path of exploration and discovery. 🙏
     
-    Let's dive into the ocean of knowledge and wisdom that Sanatana Dharma offers. From the Vedas to the Bhagavad Gita, yoga to meditation, there is much to explore.
+Let's dive into the ocean of knowledge and wisdom that Sanatana Dharma offers. From the Vedas to the Bhagavad Gita, yoga to meditation, there is much to explore.
     
-    Remember the power of sacred mantras. One such mantra is "ॐ गं गणपतये नमः" (Om Gam Ganapataye Namaha). Chant it with devotion to seek Lord Ganapati's blessings and overcome challenges.
+Remember the power of sacred mantras. One such mantra is "ॐ गं गणपतये नमः" (Om Gam Ganapataye Namaha). Chant it with devotion to seek Lord Ganapati's blessings and overcome challenges.
     
-    Have questions about dharma, rituals, yoga, or anything related to Sanatana Dharma? Ask SanatanaDharma.xyz chatbot for guidance.
+Have questions about dharma, rituals, yoga, or anything related to Sanatana Dharma? Ask SanatanaDharma.xyz chatbot for guidance.
     
-    Embrace this journey with an open mind. May it bring us closer to our true selves and lead us to a life filled with purpose, peace, and spiritual growth.`;
+Embrace this journey with an open mind. May it bring us closer to our true selves and lead us to a life filled with purpose, peace, and spiritual growth.`;
     
     const formData = new FormData();
     formData.append('From', "whatsapp:+13074486824");
