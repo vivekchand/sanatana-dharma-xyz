73f2080853169a27961cb07d146fbc15137e6678
Vivek Chand <vivekchand19@gmail.com>
2023-10-14 21:30:45 +0000

    send next

--- pages/api/send_next.ts
+++ pages/api/send_next.ts
@@ -1,23 +1,11 @@
-import { OpenAIStream } from "@/utils";
-import { kv } from "@vercel/kv";
-import { Mutex } from "async-mutex";
-import twilio from "twilio";
 import { sql } from "@vercel/postgres";
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-interface TwilioMessage {
-  sid: string;
-  // Add other properties as needed
-}
-
-
 export const config = {
   runtime: "edge"
 };
-const emailMutex = new Mutex();
-
 
 function getMessageForTemplateName(templateName: string): string {
   const templates: Record<string, string> = {
@@ -31,9 +19,37 @@ Transliteration:
 Dhritarashtra Uvacha |
 Dharmakshetre Kurukshetre Samaveta Yuyutsavah |
 Mamakah Pandavaschaiva Kimakurvata Sanjaya ||`,
-    bhagavad_gita_chapter_1_verse_2: "Bhagavad Gita Chapter 1, Verse 2 continues the conversation between Dhritarashtra and Sanjaya. Sanjaya informs Dhritarashtra that after seeing the Pandava army in battle formation, King Duryodhana approaches his teacher, Dronacharya, seeking guidance. This verse sets the stage for the subsequent events in the Bhagavad Gita, where Duryodhana's actions and mindset are contrasted with Arjuna's dilemma and Lord Krishna's teachings on duty, righteousness, and the path to spiritual enlightenment.\n\nसञ्जय उवाच |\nदृष्ट्वा तु पाण्डवानीकं व्यूढं दुर्योधनस्तदा |\nआचार्यमुपसङगम्य राजा वचनमब्रवीत् ||\nTransliteration:\nSanjaya Uvacha |\nDrishtva tu Pandavanikam vyudham Duryodhanas tada |\nAcharyam upasangamya raja vachanamabravit ||",
-    bhagavad_gita_chapter_1_verse_3: "Bhagavad Gita Chapter 1, Verse 3:\n\nIn this verse, Duryodhana addresses his teacher, Dronacharya, and draws attention to the mighty army of the Pandavas, led by their intelligent disciple, Dhrishtadyumna, the son of Drupada. Duryodhana acknowledges the strength and organization of the Pandava army, highlighting their formidable presence on the battlefield.\n\nThis verse sets the stage for the subsequent chapters, where the dialogue between Lord Krishna and Arjuna unfolds, delving into profound philosophical and spiritual teachings. The Bhagavad Gita explores various aspects of life, duty, righteousness, and the path to self-realization.\n\nपश्यैतां पाण्डुपुत्राणामाचार्य महतीं चमूम् |\nव्यूढां द्रुपदपुत्रेण तव शिष्येण धीमता ||\nTransliteration:\nPashyaitam Pandu-putranam Acharya mahatim chamoom |\nVyudham drupada-putrena tava shishyena dheemata ||",
-    ganapati_welcome: "🙏 Om Gam Ganapataye Namaha 🐘\n\nOn this auspicious day, let us begin our journey by offering our heartfelt prayers to Lord Ganapati, the remover of obstacles and the embodiment of wisdom. May his divine presence guide us on this path of exploration and discovery. 🙏\n\nLet's dive into the ocean of knowledge and wisdom that Sanatana Dharma offers. From the Vedas to the Bhagavad Gita, yoga to meditation, there is much to explore.\n\nRemember the power of sacred mantras. One such mantra is \"ॐ गं गणपतये नमः\" (Om Gam Ganapataye Namaha). Chant it with devotion to seek Lord Ganapati's blessings and overcome challenges.\n\nHave questions about dharma, rituals, yoga, or anything related to Sanatana Dharma? Ask SanatanaDharma.xyz chatbot for guidance.\n\nEmbrace this journey with an open mind. May it bring us closer to our true selves and lead us to a life filled with purpose, peace, and spiritual growth.",
+    bhagavad_gita_chapter_1_verse_2: `Bhagavad Gita Chapter 1, Verse 2 continues the conversation between Dhritarashtra and Sanjaya. Sanjaya informs Dhritarashtra that after seeing the Pandava army in battle formation, King Duryodhana approaches his teacher, Dronacharya, seeking guidance. This verse sets the stage for the subsequent events in the Bhagavad Gita, where Duryodhana's actions and mindset are contrasted with Arjuna's dilemma and Lord Krishna's teachings on duty, righteousness, and the path to spiritual enlightenment.
+    
+सञ्जय उवाच |
+दृष्ट्वा तु पाण्डवानीकं व्यूढं दुर्योधनस्तदा |
+आचार्यमुपसङगम्य राजा वचनमब्रवीत् ||
+
+Transliteration:
+Sanjaya Uvacha |
+Drishtva tu Pandavanikam vyudham Duryodhanas tada |Acharyam upasangamya raja vachanamabravit ||`,
+    bhagavad_gita_chapter_1_verse_3: `Bhagavad Gita Chapter 1, Verse 3:
+    
+In this verse, Duryodhana addresses his teacher, Dronacharya, and draws attention to the mighty army of the Pandavas, led by their intelligent disciple, Dhrishtadyumna, the son of Drupada. Duryodhana acknowledges the strength and organization of the Pandava army, highlighting their formidable presence on the battlefield.
+
+This verse sets the stage for the subsequent chapters, where the dialogue between Lord Krishna and Arjuna unfolds, delving into profound philosophical and spiritual teachings. The Bhagavad Gita explores various aspects of life, duty, righteousness, and the path to self-realization.
+
+पश्यैतां पाण्डुपुत्राणामाचार्य महतीं चमूम् |
+व्यूढां द्रुपदपुत्रेण तव शिष्येण धीमता ||
+Transliteration:
+Pashyaitam Pandu-putranam Acharya mahatim chamoom |
+Vyudham drupada-putrena tava shishyena dheemata ||`,
+    ganapati_welcome: `🙏 Om Gam Ganapataye Namaha 🐘
+    
+On this auspicious day, let us begin our journey by offering our heartfelt prayers to Lord Ganapati, the remover of obstacles and the embodiment of wisdom. May his divine presence guide us on this path of exploration and discovery. 🙏
+
+Let's dive into the ocean of knowledge and wisdom that Sanatana Dharma offers. From the Vedas to the Bhagavad Gita, yoga to meditation, there is much to explore.
+
+Remember the power of sacred mantras. One such mantra is \"ॐ गं गणपतये नमः\" (Om Gam Ganapataye Namaha). Chant it with devotion to seek Lord Ganapati's blessings and overcome challenges.
+
+Have questions about dharma, rituals, yoga, or anything related to Sanatana Dharma? Ask SanatanaDharma.xyz chatbot for guidance.
+
+Embrace this journey with an open mind. May it bring us closer to our true selves and lead us to a life filled with purpose, peace, and spiritual growth.`,
     reply_first_message: "Namaste 🙏 Welcome to the sacred journey with SanatanaDharma.xyz. You're now subscribed to receive one verse a day from the Bhagavad Gita, Vedas, Upanishads. Let the transformation begin! Please reply with \"yes\" or \"subscribe\" to receive daily messages.",
     namaste_with_book_intro: "Welcome to the enlightening journey! You're now subscribed to receive one verse a day from the Bhagavad Gita, Vedas, Upanishads. Let the transformation begin! 🙏📖✨",
     namaste_first_message: "Namaste 🙏 Welcome to the sacred journey with SanatanaDharma.xyz. You're now subscribed to receive one verse a day from the Bhagavad Gita, Vedas, Upanishads. Let the transformation begin!",
@@ -43,6 +59,36 @@ Mamakah Pandavaschaiva Kimakurvata Sanjaya ||`,
   return templates[templateName] || "Template not found.";
 }
 
+function getNextTemplate(template_name: string) {
+  const templates: Record<string, string> = {
+    first_message_on_subscription: "ganapati_welcome",
+    namaste_first_message: "ganapati_welcome",
+    namaste_with_book_intro: "ganapati_welcome",
+    reply_first_message: "ganapati_welcome",
+    ganapati_welcome: "bhagavad_gita_chapter_1_verse_1",
+    bhagavad_gita_chapter_1_verse_2: "bhagavad_gita_chapter_1_verse_3",
+    bhagavad_gita_chapter_1_verse_3: "bhagavad_gita_chapter_1_verse_4",
+    bhagavad_gita_chapter_1_verse_4: "bhagavad_gita_chapter_1_verse_5",
+    bhagavad_gita_chapter_1_verse_5: "bhagavad_gita_chapter_1_verse_6",
+    bhagavad_gita_chapter_1_verse_6: "bhagavad_gita_chapter_1_verse_7",
+    bhagavad_gita_chapter_1_verse_7: "bhagavad_gita_chapter_1_verse_8",
+    bhagavad_gita_chapter_1_verse_8: "bhagavad_gita_chapter_1_verse_9",
+    bhagavad_gita_chapter_1_verse_9: "bhagavad_gita_chapter_1_verse_10",
+    bhagavad_gita_chapter_1_verse_10: "bhagavad_gita_chapter_1_verse_11",
+    bhagavad_gita_chapter_1_verse_11: "bhagavad_gita_chapter_1_verse_12",
+    bhagavad_gita_chapter_1_verse_12: "bhagavad_gita_chapter_1_verse_13",
+    bhagavad_gita_chapter_1_verse_13: "bhagavad_gita_chapter_1_verse_14",
+    bhagavad_gita_chapter_1_verse_14: "bhagavad_gita_chapter_1_verse_15",
+    bhagavad_gita_chapter_1_verse_15: "bhagavad_gita_chapter_1_verse_16",
+    bhagavad_gita_chapter_1_verse_16: "bhagavad_gita_chapter_1_verse_17",
+    bhagavad_gita_chapter_1_verse_17: "bhagavad_gita_chapter_1_verse_18",
+    bhagavad_gita_chapter_1_verse_18: "bhagavad_gita_chapter_1_verse_19",
+    bhagavad_gita_chapter_1_verse_19: "bhagavad_gita_chapter_1_verse_20",
+    bhagavad_gita_chapter_1_verse_20: "bhagavad_gita_chapter_1_verse_21",
+    bhagavad_gita_chapter_1_verse_21: "bhagavad_gita_chapter_1_verse_22",
+  };
+}
+
 async function sendWhatsappMessage(template:string) {
   try {
     const message = getMessageForTemplateName(template);
@@ -50,23 +96,24 @@ async function sendWhatsappMessage(template:string) {
       SELECT * FROM subscriber WHERE lastSentTemplate = ${template};
     `;
     const { rows } = await selectQuery;
-        console.log(rows);
-    // console.log("inside sendWhatsappMessage");
-    // console.log(phone);
-    // console.log(template);
-    // console.log(message);
-    // const formData = new FormData();
-    // formData.append('From', "whatsapp:+13074486824");
-    // formData.append('To', "whatsapp:" + phone);
-    // formData.append('Body', message);
-    
-    // const response = await fetch(`https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Authorization': `Basic ${Buffer.from(`${accountSid}:${authToken}`).toString('base64')}`,
-    //   },
-    //   body: formData,
-    // });
+    for (const row of rows) {
+      const phone = row.phoneNumber;
+      console.log("inside sendWhatsappMessage");
+      console.log(phone);
+      console.log(template);
+      console.log(message);
+      const formData = new FormData();
+      formData.append('From', "whatsapp:+13074486824");
+      formData.append('To', "whatsapp:" + phone);
+      formData.append('Body', message);
+      
+      const response = await fetch(`https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${accountSid}:${authToken}`).toString('base64')}`,
+        },
+        body: formData,
+      });    }
 
   } catch (error) {
     console.error("An error occurred:", error);
