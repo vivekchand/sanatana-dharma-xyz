7c9363b6f3e797068a1e133536df591be48b9d00
Vivek Chand <vivekchand19@gmail.com>
2023-10-17 11:25:07 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,9 +39,9 @@ export default function Home() {
     if(subscribedTime) {
       return;
     }
-    if(messages.length < 1) {
-      return;
-    }
+    // if(messages.length < 1) {
+    //   return;
+    // }
     if (!lastDismissedTime || Date.now() - parseInt(lastDismissedTime) > 5 * 60 * 1000) {
       setTimeout(() => {
         setShowSubscriptionPopup(true);
