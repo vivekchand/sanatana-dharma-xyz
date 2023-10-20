0c70a7a5de9abaed33773822383d8f37e1eec99c
Vivek Chand <vivekchand19@gmail.com>
2023-10-20 22:28:06 +0000

    send email

--- pages/api/send_email.js
+++ pages/api/send_email.js
@@ -7,20 +7,20 @@ const mailerSend = new MailerSend({
 });
 
 const handler = async (req, res) => {
-    // const emailHtml = render(<Email url="https://sanatanadharma.xyz" />);
+    const emailHtml = render(<Email url="https://sanatanadharma.xyz" />);
 
-    // const sentFrom = new Sender("namaste@sanatanadharma.xyz", "SanatanaDharma.xyz");
-    // const recipients = [
-    //     new Recipient("vivekchand19@gmail.com", "Vivek Chand")
-    // ];
+    const sentFrom = new Sender("namaste@sanatanadharma.xyz", "SanatanaDharma.xyz");
+    const recipients = [
+        new Recipient("vivekchand19@gmail.com", "Vivek Chand")
+    ];
 
-    // const emailParams = new EmailParams()
-    //     .setFrom(sentFrom)
-    //     .setTo(recipients)
-    //     .setSubject("Namaskaram!")
-    //     .setHtml(emailHtml)
+    const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject("Namaskaram!")
+        .setHtml(emailHtml)
 
-    // await mailerSend.email.send(emailParams);
+    await mailerSend.email.send(emailParams);
     return res.status(200).send("Email Sent!!!");
 }
 
