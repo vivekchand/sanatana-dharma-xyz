9d6ec5ac1e7953616ce9f876cd9cc8282ad4f727
Vivek Chand <vivekchand19@gmail.com>
2023-10-20 21:41:57 +0000

    add files

---
+++ pages/api/send_email.js
@@ -0,0 +1,26 @@
+import { Email } from '../email';
+import { render } from '@react-email/render';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY || '',
+});
+
+const handler = async (req) => {
+    const emailHtml = render(<Email url="https://sanatanadharma.xyz" />);
+
+    const sentFrom = new Sender("namaste@sanatanadharma.xyz", "SanatanaDharma.xyz");
+    const recipients = [
+        new Recipient("vivekchand19@gmail.com", "Vivek Chand")
+    ];
+
+    const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject("Namaskaram!")
+        .setHtml(emailHtml)
+
+    mailerSend.email.send(emailParams);
+}
+
+export default handler;
