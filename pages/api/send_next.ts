68753031e06b193b564c6a1b1fad7386b2618fc6
Vivek Chand <vivekchand19@gmail.com>
2023-10-22 10:00:23 +0000

    stop sending

--- pages/api/send_next.ts
+++ pages/api/send_next.ts
@@ -367,6 +367,7 @@ async function sendNextEmail(email:string, template:string) {
   const response = await fetch("https://sanatanadharma.xyz/api/send_email?template="+"abc"+"&lang=en&email="+email, {
     method: 'GET'
   });
+  console.log("The response is:");
   console.log(response);
   if(response.ok) {
     const insertQuery = sql`
