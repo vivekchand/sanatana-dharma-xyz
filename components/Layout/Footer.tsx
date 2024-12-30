901ca84d3ddd924b6523fa22ec79335a0dce87a5
Vivek Chand <vivekchand19@gmail.com>
2024-12-30 19:51:12 +0100

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -32,12 +32,7 @@ export const Footer: FC = () => {
       {/* <Link href="/privacy">
         <a className="text-neutral-500 hover:text-primary-500">Privacy Policy</a>
       </Link> */}
-      <a
-        onClick={handleOpenSubscriptionPopup}
-        className="text-neutral-500 hover:text-primary-500 cursor-pointer"
-      >
-        Subscribe
-      </a>
+      {/* Subscribe link removed */}
       <Link href="https://www.producthunt.com/posts/sanatanadharma-xyz?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sanatanadharma-xyz" target="_blank" rel="noopener noreferrer">
         <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=416280&theme=neutral" alt="SanatanaDharma.xyz - Sanatana Dharma Chatbot: Revealing Our Heritage 🕉️ | Product Hunt" style={{ width: '250px', height: '34px' }} width="250" height="54" />
       </Link>
